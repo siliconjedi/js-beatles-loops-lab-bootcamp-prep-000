3390fc9037ccf1c884e16c9ded56d1cefd834c28
Robert D Gauthier <rgauthie@gmail.com>
2018-03-16 11:42:06 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -9,7 +9,8 @@ function theBeatlesPlay (musicians, instruments) {
 function johnLennonFacts (facts){
   let i = 0;
   while (i < facts.length) {
-    facts[i] = facts[i] + "!!!"
+    facts[i] = facts[i] + "!!!";
+    i++;
   }
   return facts;
 }
